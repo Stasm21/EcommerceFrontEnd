2424dff260640d5cd8c93868d95c4b4512553ae5
Kory Attleson <kory.attleson@gmail.com>
2021-09-30 15:40:55 -0500

    Attempted to re-renderweb page when making a product. Does not work atm

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -20,7 +20,7 @@ class App extends Component {
             categories: [],
             registeredUser: [],
             currentUser: [],
-            products: []
+            products: [],
          }
     }
 
