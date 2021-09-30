e5e26d877f178ec833862c84e58d5eb1b77fd12d
treveon edwards <fasuke322@gmail.com>
2021-09-30 15:46:42 -0400

    merge

--- ecommerce/src/components/ProductTable/ProductTable.jsx
+++ ecommerce/src/components/ProductTable/ProductTable.jsx
@@ -1,62 +1,39 @@
-
-import ProductSearch from './ProductSearch';
-import React, { Component } from 'react';
+import React from 'react';
 
 
-class Products  extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            products:[],
-            "productID": 0,
-            "name": "",
-            "price": 0,
-            "description": "",
-            "averageRating": 0,
-            "categoryID": 0,
-            "category": "",
-            "userId": null,
-            "user": null
-         }
-         
-    }
-    
-    
-    axios.get('http://localhost:44394/api/product')
-    
-    
-    render() {
-        return ( 
-            <div>
-                
-                <table>
+const ProductTable  = (props) => {
+    return (
+        <div className = "center">
+            <br/><br/>
+            <table>
                 <thead>
-
-                    
                     <tr>
-                        <th>name</th>
+                    <th>name</th>
                         <th>price</th>
                         <th>description</th>
                         <th>averageRating</th>
+                        <th>Category</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {this.state.products.map(products =>(
-                        <tr key={products.ProductID}>
-                            <td>{products.name}</td>
-                            <td>{products.price}</td>
-                            <td>{products.description}</td>
-                            <td>{products.averageRating}</td>
-                             
-                        </tr>
-                        
-                    ))}
+                {props.products.map((product) => {
+                    return(
+                            <tr key = {product.id}>
+                                <td>{product.name}</td>
+                                <td>{product.description}</td>
+                                <td>{product.averageRating}</td>
+                                <td>{product.price}</td>
+                                <td>{product.category}</td>
+
+                                
+                                <td><button onClick = {()=> props.delete(product.id)}>Delete product</button></td>
+                            </tr>
+                    );
+                })}
                 </tbody>
-
             </table>
-            </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default Products;
+ 
+export default ProductTable;
